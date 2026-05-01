1b7a2a0295f18e99ed65c29ab6a9006168bd8d2f
KalmiaLatifolia <lauraberman11@gmail.com>
2026-05-01 11:03:42 -0500

    map all the different structural diversity maps

--- code/ForestDiversityMap.R
+++ code/ForestDiversityMap.R
@@ -51,7 +51,7 @@ ggplot() +
 GEDI_mean <- rast("Bwindi_GIT/rasters/Forest_Diversity_CAF_WesternUS_2481_1-20260430_175616/ForStruc_GEDI_AFRICA_Mean_rh98_rh25_pai_cover_fhd_1km.tif")
 
 # load Bwindi shapefile
-Bwindi <- st_read("Bwindi_GIT/rasters/WDPA_WDOECM_May2026_Public_61609_shp/WDPA_WDOECM_May2026_Public_61609_shp_0")
+Bwindi <- st_read("Bwindi_GIT/shapefiles/WDPA_WDOECM_May2026_Public_61609_shp/WDPA_WDOECM_May2026_Public_61609_shp_0")
 mapview(Bwindi)
 
 # Transform extent object to match raster
@@ -111,16 +111,91 @@ ggplot() +
 
 
 
+# load std raster data -------------------------------------------------------------
+GEDI_std <- rast("Bwindi_GIT/rasters/Forest_Diversity_CAF_WesternUS_2481_1-20260430_175616/ForStruc_GEDI_AFRICA_std_rh98_rh25_pai_cover_fhd_1km.tif")
 
 
+# crop the raster
+GEDI_std_crop <- crop(GEDI_std, ext)
 
 
+# Plot canopy height 
+ggplot() +
+  geom_spatraster(data = GEDI_std_crop$`RH98 relative height 98th percentile`) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "std RH98 height (m)")
+
+# Plot mid-canopy height 
+ggplot() +
+  geom_spatraster(data = GEDI_std_crop$`RH25 relative height 25th percentile`) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "std RH25 height (m)")
+
+# Plot PAI
+ggplot() +
+  geom_spatraster(data = GEDI_std_crop$`PAI plant area index`) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "std Plant area index")
+
+# Plot PAI
+ggplot() +
+  geom_spatraster(data = GEDI_std_crop$`canopy cover`) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "std canopy cover")
+
+# Plot PAI
+ggplot() +
+  geom_spatraster(data = GEDI_std_crop$`foliage height diversity`) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "std foliage height diversity")
 
 
+# load structural diversity raster data -------------------------------------------------------------
+GEDI_FSTR <- rast("Bwindi_GIT/rasters/Forest_Diversity_CAF_WesternUS_2481_1-20260430_175616/ForStruc_GEDI_AFRICA_StructuralDiversity_1km.tif")
 
+# crop the raster
+GEDI_FSTR_crop <- crop(GEDI_FSTR, ext)
 
+# Plot structural richness
+ggplot() +
+  geom_spatraster(data = GEDI_FSTR_crop$richness) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "structural richness")
 
+# Plot structural richness
+ggplot() +
+  geom_spatraster(data = GEDI_FSTR_crop$evenness) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "evenness")
 
-
+# Plot structural divergence
+ggplot() +
+  geom_spatraster(data = GEDI_FSTR_crop$divergence) +
+  theme_minimal() +
+  geom_sf(data = Bwindi, fill = NA, color = "black", linewidth = 1.2) +
+  annotation_scale(location = "br") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering) +
+  labs(fill = "divergence")
 
 
